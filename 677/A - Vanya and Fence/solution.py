3fbb5efee0a32e51dbb37971a0b5d6663040d8cf
Lalithyapalla <pallalalithya25@gmail.com>
2026-06-07 15:08:48 +0530

    Add Vanya and Fence [A] from Codeforces

---
+++ 677/A - Vanya and Fence/solution.py
@@ -0,0 +1,9 @@
+n, h = map(int, input().split())
+heights = list(map(int, input().split()))
+count = 0
+for p in heights:
+    if p <= h:
+        count += 1
+    else:
+        count += 2
+print(count)
